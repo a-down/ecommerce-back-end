a7ace88f719ee4ae32486fad3686655b75dead6d
a-down <136649391+a2downing@users.noreply.github.com>
2023-08-08 15:35:51 -0500

    models and associations initial setup

--- models/index.js
+++ models/index.js
@@ -4,13 +4,32 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: null,
+})
 
-// Categories have many Products
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+})
 
 // Products belongToMany Tags (through ProductTag)
+Products.belongToMany(Tags, {
+  through: {
+    model: 'product_tags',
+    unique: false
+  },
+  as: 'products_tags'
+})
 
 // Tags belongToMany Products (through ProductTag)
+Tags.belongToMany(Products, {
+  through: {
+    model: 'product_tags',
+    unique: false
+  },
+  as: 'tags_products'
+})
 
 module.exports = {
   Product,
