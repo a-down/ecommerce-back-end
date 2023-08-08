0aae8324bf592fa8c39a2a3c17e2a95447f32aeb
a-down <136649391+a2downing@users.noreply.github.com>
2023-08-08 18:06:17 -0500

    add new product feature

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -42,17 +42,8 @@ router.get('/:id', async (req, res) => {
 
 // post new product
 router.post('/', async (req, res) => {
-  /* req.body should look like this...
-    {
-      product_name: "Basketball",
-      price: 200.00,
-      stock: 3,
-      tagIds: [1, 2, 3, 4]
-    }
-  */
-  Product.create(req.body)
+  const newData = Product.create(req.body)
     .then((product) => {
-      // if there's product tags, we need to create pairings to bulk create in the ProductTag model
       if (req.body.tagIds.length) {
         const productTagIdArr = req.body.tagIds.map((tag_id) => {
           return {
@@ -62,10 +53,9 @@ router.post('/', async (req, res) => {
         });
         return ProductTag.bulkCreate(productTagIdArr);
       }
-      // if no product tags, just respond
-      res.status(200).json(product);
+      res.status(200).json(newData);
     })
-    .then((productTagIds) => res.status(200).json(productTagIds))
+    .then((productTagIds) => res.status(200).json({message: 'New product created.'}))
     .catch((err) => {
       console.log(err);
       res.status(400).json(err);
