355614a75ce14145ee26bf26983339a57ce9cf2d
a-down <136649391+a2downing@users.noreply.github.com>
2023-08-08 17:06:16 -0500

    categories put and display new

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -15,11 +15,24 @@ router.get('/:id', async (req, res) => {
 
 router.post('/', async (req, res) => {
   const data = await Category.create({category_name: req.body.category_name}).catch((err) => res.status(500).json(err))
-  res.status(200).json({message: "Category Created", newCategory: data})
+  res.status(200).json({message: "Category Created", data})
 });
 
 router.put('/:id', async (req, res) => {
-  // update a category by its `id` value
+  const data = await Category.update(
+    {
+      category_name: req.body.category_name,
+    },
+    {
+      where: {
+        id: req.params.id,
+      }
+    })
+    .catch((err) => res.status(500).json(err))
+
+  const newData = await Category.findByPk(req.params.id, {include: [{model: Product}]} ).catch((err) => res.status(500).json(err))
+
+  res.status(200).json({message: 'Category updated.', newData})
 });
 
 router.delete('/:id', async (req, res) => {
