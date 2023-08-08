a03327293f83c5796c40529ad499edd954a994e7
a-down <136649391+a2downing@users.noreply.github.com>
2023-08-08 17:16:33 -0500

    check if category exists on get :id

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,23 +1,37 @@
 const router = require('express').Router();
 const { Category, Product } = require('../../models');
 
-// The `/api/categories` endpoint
 
+// get all categories
 router.get('/', async (req, res) => {
   const data = await Category.findAll( {include: [{model: Product}]} ).catch((err) => res.status(500).json(err))
   res.status(200).json(data)
 });
 
+
+// get category by id
 router.get('/:id', async (req, res) => {
-  const data = await Category.findByPk(req.params.id, {include: [{model: Product}]} ).catch((err) => res.status(500).json(err))
-  res.status(200).json(data)
+  try {
+    const data = await Category.findByPk(req.params.id, {include: [{model: Product}]} )
+    if (!data) {
+      res.status(404).json({ message: "No category with this id."})
+    }
+    res.status(200).json(data)
+    } catch (err) {
+      res.status(500).json(err)
+    }
+    
 });
 
+
+// post new category
 router.post('/', async (req, res) => {
   const data = await Category.create({category_name: req.body.category_name}).catch((err) => res.status(500).json(err))
-  res.status(200).json({message: "Category Created", data})
+  res.status(200).json({message: "Category created", data})
 });
 
+
+// update category by id
 router.put('/:id', async (req, res) => {
   const data = await Category.update(
     {
@@ -35,8 +49,15 @@ router.put('/:id', async (req, res) => {
   res.status(200).json({message: 'Category updated.', newData})
 });
 
+
+// delete category by id
 router.delete('/:id', async (req, res) => {
-  // delete a category by its `id` value
+  const data = await Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  }).catch((err) => res.status(500).json(err))
+  res.status(200).json({message: 'Category deleted.'})
 });
 
 module.exports = router;
