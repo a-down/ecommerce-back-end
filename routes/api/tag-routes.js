c127f1a6dbd93f15f6d724d2a6184d93b9258902
a-down <136649391+a2downing@users.noreply.github.com>
2023-08-08 20:54:19 -0500

    GET by id feature

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -17,8 +17,22 @@ router.get('/', async (req, res) => {
 });
 
 router.get('/:id', async (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+  try {
+    const data = await Tag.findByPk(req.params.id, {
+      include: [{
+        model: Product,
+        through: ProductTag,
+        as: 'tags_products' // ex 2 did work
+       },
+      ]})
+    if (!data) {
+      res.status(404).json({message: 'No tag with this id.'})
+      return;
+    } 
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
 router.post('/', async (req, res) => {
